220012a8f639c2bfdcd0b0bd500f32fac76db8b8
Lam Lay <lam.lay@wallawalla.edu>
2018-01-31 01:09:03 +0000

    autosave

---
+++ project1.cpp
@@ -0,0 +1,12 @@
+/*************************************************************************
+ * File Name:       project1.cpp
+ * Name:            lam Lay
+ * Course:          CPTR 142
+ * Date:            January 10, 2018
+ * 
+ */
+
+#include <iostream>  // for cin and cout
+#include <stdlib.h>  // for exit
+using namespace std;
+#include <limits> // for invalid input
